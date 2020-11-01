a42d83f4ba47aa9281b8fb297b9a0352be725453
Sudip Ghosh <ghoshsudip084@gmail.com>
2020-11-01 23:51:32 +0530

    Change 9

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+/*import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 function generateCodeFromObject(obj) {
@@ -47,6 +47,66 @@ function generateCodeFromObject(obj) {
   };
 
   return jsonToJSX(obj).toString();
+}
+
+module.exports = generateCodeFromObject;
+
+// ReactDOM.render(<App />, document.getElementById("root"))*/
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+const toTitleCase = (str) => {
+  return str
+    .replace(/\w\S*/g, function (txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    })
+    .replace(" ", "");
+};
+
+function toCamelCase(str) {
+  str = str.replace("-", " ");
+  return str
+    .replace(/\s(.)/g, function ($1) {
+      return $1.toUpperCase();
+    })
+    .replace(/\s/g, "")
+    .replace(/^(.)/, function ($1) {
+      return $1.toLowerCase();
+    });
+}
+
+const jsonToJSX = (data) => {
+  let initial = `<${data.name}`;
+
+  if (data.style !== undefined && Object.keys(data.style).length > 0) {
+    let styleKeys = Object.keys(data.style);
+    initial += " style={{";
+
+    for (let i = 0; i < styleKeys.length; i++) {
+      initial += `${toCamelCase(styleKeys[i])}:"${data.style[styleKeys[i]]}",`;
+    }
+    initial = `${initial.substr(0, initial.length - 1)}}}`;
+  }
+
+  if (data.children !== undefined && data.children.length > 0) {
+    initial += ">\n";
+
+    for (let i = 0; i < data.children.length; i++) {
+      initial += jsonToJSX(data.children[i]);
+    }
+
+    initial = `${initial}</${data.name}>`;
+  } else {
+    initial += "/>";
+  }
+
+  return initial;
+};
+
+function generateCodeFromObject(obj) {
+  return jsonToJSX(obj);
 }
 
 module.exports = generateCodeFromObject;
